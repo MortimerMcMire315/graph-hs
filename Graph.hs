0462804458968e35be0633b782b2d67ca2b94a79
Seth A. Yoder <seth.a.yoder@gmail.com>
2015-04-06 12:16:41 -0400

    Adding functions to change edge weights.

--- Graph.hs
+++ Graph.hs
@@ -1,19 +1,28 @@
 module Graph where
 
-type WeightedEdge v e = ((v,v),e)
-data WeightedGraph v e = WeightedGraph {vertices :: [v], edges :: [WeightedEdge v e] } deriving (Show, Eq)
+type Edge v e = ((v,v),e)
+data Graph v e = Graph {vertices :: [v], edges :: [Edge v e] } deriving (Show, Eq)
 
-g1 :: WeightedGraph Integer Integer
-g1 = WeightedGraph [1, 2, 3, 4, 5] [((1,2),0), ((3,2),0)]
+g1 :: Graph Integer Integer
+g1 = Graph [1, 2, 3, 4, 5] [((1,2),0), ((3,2),0)]
 
-assignWeights :: WeightedGraph v e -> [e] -> Maybe (WeightedGraph v e)
-assignWeights (WeightedGraph verts edges) weightList@(x:xs)
+assignWeights :: Graph v e -> [e] -> Maybe (Graph v e)
+assignWeights (Graph verts edges) weightList@(x:xs)
     | length edges /= length weightList = Nothing
-    | otherwise = Just (WeightedGraph verts (zipWith (\x y -> (fst x, y)) edges weightList))
+    | otherwise = Just (Graph verts (zipWith (\x y -> (fst x, y)) edges weightList))
 
-addWeightedEdge :: (Eq v) => WeightedGraph v e -> WeightedEdge v e -> Maybe (WeightedGraph v e)
-addWeightedEdge (WeightedGraph vs es) e@((v1,v2),_)
+addEdge :: (Eq v) => Graph v e -> Edge v e -> Maybe (Graph v e)
+addEdge (Graph vs es) e@((v1,v2),_)
     | not (elem v1 vs) || not (elem v2 vs) = Nothing
-    | otherwise = Just (WeightedGraph vs (e:es))
+    | otherwise = Just (Graph vs (e:es))
+
+setEdgeWeight :: Edge v e -> e -> Edge v e
+setEdgeWeight (vs,w) w' = (vs,w')
+
+setEdgeWeightG :: (Eq v, Eq e) => Graph v e -> (v,v) -> e -> Maybe (Graph v e)
+setEdgeWeightG (Graph vs es@(x:xs)) toChange w'
+    | null es = Nothing
+    | fst x == toChange = Just $ Graph vs $ setEdgeWeight x w':xs
+    | otherwise = setEdgeWeightG (Graph vs xs) toChange w'
 
 main = putStrLn $ show g1
