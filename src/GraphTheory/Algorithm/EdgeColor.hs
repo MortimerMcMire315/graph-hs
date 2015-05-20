636d523f81557dda63b3023eeaa2a030a21dba68
Seth Yoder <seth.a.yoder@gmail.com>
2015-05-20 01:04:37 -0400

    Remove debugging function calls.

--- src/GraphTheory/Algorithm/EdgeColor.hs
+++ src/GraphTheory/Algorithm/EdgeColor.hs
@@ -4,25 +4,21 @@ import GraphTheory.Graph (Edge (..), Graph, vertices, edges, incidentEdges, degr
 import GraphTheory.Algorithm.DFS (getComponents)
 import Data.List ((\\), intersect, elemIndex, nub)
 import qualified Data.Map as M
-import Debug.Trace (trace)
 import GraphTheory.Misc.Infinity (Inf, Infinitable (..))
 import Data.Maybe (fromJust)
 
 
 {-----====== Setup ======-----}
-deltaPlusOneColor :: (Ord v, Ord w, Eq w, Eq v, Show w, Show v, Num w) => 
+deltaPlusOneColor :: (Ord v, Eq w, Eq v) => 
                      Graph v w Integer -> Graph v w Integer
 deltaPlusOneColor g = inductLoop g g'
     where g' = g {vertices = [] , edges = []}
 
-inductLoop :: (Ord v, Ord w, Eq w, Eq v, Show w, Show v, Num w) => 
+inductLoop :: (Ord v, Eq w, Eq v) => 
               Graph v w Integer -> Graph v w Integer -> Graph v w Integer
 inductLoop g gp
     | null $ edges g = gp
-    | otherwise = --inductLoop g {edges=restEdges} $ doDeltaPlusOneColor nextgp
-                  if isValidColoring (doDeltaPlusOneColor nextgp) 
-                  then {--trace (show $ doDeltaPlusOneColor nextgp) $ --} inductLoop g {edges=restEdges} $ doDeltaPlusOneColor nextgp
-                  else error "WHOOP"
+    | otherwise = inductLoop g {edges=restEdges} $ doDeltaPlusOneColor nextgp
     where nextgp = gp {edges = gp_edges, vertices = gp_vertices}
           (firstEdge:restEdges) = edges g
           gp_edges = (firstEdge {edgeData = -1}) : (edges gp)
@@ -33,7 +29,7 @@ inductLoop g gp
 
 
 {-----====== Substance (in sort-of execution order) ======-----}
-doDeltaPlusOneColor :: (Ord v, Ord w, Eq w, Show w, Show v, Num w) => 
+doDeltaPlusOneColor :: (Ord v, Eq w) => 
                        Graph v w Integer -> Graph v w Integer
 doDeltaPlusOneColor g = if (not . null) missingAtBoth 
                         then easyColor g (head missingAtBoth) 
@@ -49,7 +45,7 @@ easyColor g c = g {edges = (new_e:es)}
     where (e:es) = edges g
           new_e = e {edgeData = c}
 
-hardColor :: (Ord v, Ord w, Eq v, Eq w, Show v, Show w, Num w) => 
+hardColor :: (Ord v, Eq v, Eq w) => 
              Graph v w Integer -> Edge v w Integer -> [Integer] -> Graph v w Integer
 hardColor g e gColors = colorSequence
     where (v0,v1) = endpoints e
@@ -60,29 +56,26 @@ hardColor g e gColors = colorSequence
           colorSequence = makeColorSequence g gColors [c0,c1] [(v0,v1)]
 
 
-makeColorSequence :: (Ord v, Ord w, Eq v, Eq w, Show v, Show w, Num w) => 
+makeColorSequence :: (Ord v, Eq v, Eq w) => 
                      Graph v w Integer -> [Integer] ->  [Integer] -> [(v,v)] -> Graph v w Integer
 makeColorSequence g gColors cSeq eSeq 
     | null $ vertexWithColor c_i = hcCase1 g cSeq eSeq
     | elem vk (map snd eSeq) = hcCase2 g cSeq eSeq vk
     | otherwise = makeColorSequence g gColors next_cSeq next_eSeq 
     where c_i = last cSeq
-          v0 = (fst . head) eSeq                        --TODO This won't work on directed graphs.
+          v0 = (fst . head) eSeq
           vi_plus_1 = head (vertexWithColor c_i)
           vertexWithColor c = take 1 $ map (otherVertex v0 . endpoints) $ filter (\e -> edgeData e == c) $ incidentEdges g v0
           vk = head $ vertexWithColor c_i
           next_cSeq = cSeq ++ [(head $ colorsMissingAt g gColors vi_plus_1)]
           next_eSeq = eSeq ++ [(v0, vi_plus_1)]
-          --traceStuff = "\n\nRECURSE!!!\n" ++ (show g) ++ "\n\ncSeq=" ++ (show cSeq) ++ "; eSeq=" ++ (show eSeq) ++ "; vk=" ++ (show $ vertexWithColor c_i)
 
-
-hcCase1 :: (Show v, Show w, Eq v, Eq w, Num w) => 
+hcCase1 :: (Eq v, Eq w) => 
            Graph v w Integer -> [Integer] -> [(v,v)] -> Graph v w Integer
 hcCase1 g cSeq eSeq = foldl setEdgeColor g (zip eSeq (tail cSeq)) --Use tail so that C0 doesn't match up with (v0, v1).
     where buildEdge (v1,v2) cn = Edge (v1,v2) 1 cn
 
-
-hcCase2 :: (Show v, Show w, Ord v, Ord w, Eq v, Eq w, Num w) => 
+hcCase2 :: (Ord v, Eq v, Eq w) => 
            Graph v w Integer -> [Integer] -> [(v,v)] -> v -> Graph v w Integer
 hcCase2 g cSeq eSeq vk = kempeBranch recoloredG cSeq eSeq vk
     where recoloredG = setEdgeColor (setEdgeColors g zipped) ((v0,vk),-1)
@@ -90,24 +83,20 @@ hcCase2 g cSeq eSeq vk = kempeBranch recoloredG cSeq eSeq vk
           vj = (snd . last) eSeq
           eSeqBeforevk = takeWhile (\x -> snd x /= vk) eSeq
           zipped = zip eSeqBeforevk $ tail cSeq --Edges to recolor in the first step, paired with their new colors
-          tracestuff = show cSeq ++ " " ++ show eSeq ++ " " ++ show vk ++ " " ++ show eSeqBeforevk ++ "\n" ++ show recoloredG
 
 
-kempeBranch :: (Show v, Show w, Ord v, Ord w, Eq v, Eq w, Num w) => 
+kempeBranch :: (Ord v, Eq v, Eq w) => 
                Graph v w Integer -> [Integer] -> [(v,v)] -> v -> Graph v w Integer
 kempeBranch g cSeq eSeq vk = if notElem v0 (map fst $ h vk) && notElem v0 (map snd $ h vk)
-                             then {-- trace traceStuff $ --} branchA g (h vk) c0 cj v0 vk
-                             else trace "BRANCH B" $ branchB g (h vj) c0 cj cSeq eSeq v0 vk
+                             then branchA g (h vk) c0 cj v0 vk
+                             else branchB g (h vj) c0 cj cSeq eSeq v0 vk
     where v0 = (fst . head) eSeq; vj = (snd . last) eSeq 
           c0 = head cSeq; cj = last cSeq
           h = kempeCompEdges g c0 cj
           h_vk = h vk; h_vj = h vj;
-          traceStuff = ("\n\ncSeq =" ++ (show cSeq) ++ "\neSeq = " ++ (show eSeq) ++ "\nv0 = " ++ (show v0) ++ "\nvk = " ++ (show vk) ++
-                        "\nvj = " ++ (show vj) ++ "\nC0 = " ++ (show c0) ++ "\nCj = " ++ (show cj) ++ 
-                        "\nKempe subgraph = " ++ (show $ kempeSubgraph g c0 cj) ++ "\nHvj = " ++ (show h_vj) ++ "\nHvk = " ++ (show h_vk) ++ "\n")
 
 --Return the edges in the kempe component characterized by the given vertex
-kempeCompEdges :: (Show v, Show w, Ord v, Ord w, Eq v, Eq w, Num w) => 
+kempeCompEdges :: (Ord v, Eq v, Eq w) => 
                   Graph v w Integer -> Integer -> Integer -> v -> [(v,v)]
 kempeCompEdges g c0 cj vert = map endpoints $ filter kempeFilter (edges kempe)
     where kempeFilter e = let k = kempeCompVertices vert; (v1,v2) = endpoints e 
@@ -118,20 +107,16 @@ kempeCompEdges g c0 cj vert = map endpoints $ filter kempeFilter (edges kempe)
           kempe = kempeSubgraph g c0 cj
 
                        
-branchA g hvk c0 cj v0 vk = trace "BRANCH A:\n" result
-    where result = setEdgeColor (interchangeColors g hvk c0 cj) ((v0,vk),c0)
+branchA g hvk c0 cj v0 vk = setEdgeColor (interchangeColors g hvk c0 cj) ((v0,vk),c0)
 
-branchB :: (Show w, Show v, Eq v, Eq w) => Graph v w Integer -> [(v,v)] -> Integer -> Integer -> [Integer] -> [(v,v)] -> v -> v -> Graph v w Integer
-branchB g hvj c0 cj cSeq eSeq v0 vk = if (length cSeq_end) /= (length eSeq_end) then error errorMsg
-                                      else setEdgeColor interchanged_g ((v0,vj),c0)
+branchB :: (Eq v, Eq w) => Graph v w Integer -> [(v,v)] -> Integer -> Integer -> [Integer] -> [(v,v)] -> v -> v -> Graph v w Integer
+branchB g hvj c0 cj cSeq eSeq v0 vk = setEdgeColor interchanged_g ((v0,vj),c0)
     where k = fromJust $ elemIndex vk (map snd eSeq)
           vj = (snd . last) eSeq
           cSeq_end = init $ drop (k + 1) cSeq
           eSeq_end = init $ drop k eSeq
           recolored_g = setEdgeColors g (zip eSeq_end cSeq_end)
           interchanged_g = interchangeColors recolored_g hvj c0 cj
-          errorMsg = ("eSeq: " ++ show eSeq ++ " ; cSeq: " ++ show cSeq ++ " ; eSeq_end: " ++ show eSeq_end ++ " ; cSeq_end: " ++ 
-                      show cSeq_end ++ "\nvk: " ++ show vk ++ "k: " ++ show k)
 
 {-----====== Helpers ======-----}
 otherVertex :: (Eq v) => v -> (v,v) -> v
